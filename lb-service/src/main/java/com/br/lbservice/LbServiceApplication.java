d1038784ea38129e5957143df5c4920e4d777f9e
Michelletxr <michelleteixeiramartins@gmail.com>
2023-06-18 10:05:20 -0300

    adicionando load balancer

---
+++ lb-service/src/main/java/com/br/lbservice/LbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.lbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbServiceApplication.class, args);
+	}
+
+}
---
+++ lb-service/src/test/java/com/br/lbservice/LbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.lbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
